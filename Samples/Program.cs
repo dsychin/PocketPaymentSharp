89d3d5e503764048b4cf4c1f817bd70eacda9258
Desmond Chin <chin.sy.des@gmail.com>
2020-11-17 18:03:01 +0800

    add check for required env

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -10,7 +10,20 @@ namespace Samples
             var clientId = Environment.GetEnvironmentVariable("POCKET_CLIENT_ID");
             var secret = Environment.GetEnvironmentVariable("POCKET_SECRET");
 
+            if (string.IsNullOrWhiteSpace(clientId))
+            {
+                Console.WriteLine("\"POCKET_CLIENT_ID\" environment variable is missing!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(secret))
+            {
+                Console.WriteLine("\"POCKET_SECRET\" environment variable is missing!");
+                return;
+            }
+
             var client = new PocketClient(clientId, secret, Platform.Test);
+            CheckAuthExample(client);
         }
 
         static void CheckAuthExample(PocketClient client)
